c5b29a55df3910fa50315bd71044fe65bf546d6d
15860159070 <15860159070@cloudtogo.cn>
2020-01-07 13:20:26 +0000

    Cloud To Go is Amazing!

--- src/main/java/cn/cloudtogo/demo/DemoApplication.java
+++ src/main/java/cn/cloudtogo/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 //  sfdsfdsffdf
+// dfsfsfd
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
