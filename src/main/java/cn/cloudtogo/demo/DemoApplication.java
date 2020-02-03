1437bfd663ba4daf9d9565309dcb91cc5b7e54ff
xingyao.liu <xingyao.liu@cloudtogo.cn>
2020-02-03 17:07:21 +0800

    ccc

--- src/main/java/cn/cloudtogo/demo/DemoApplication.java
+++ src/main/java/cn/cloudtogo/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-// gjgh  hhhshfhsdhfh
+// gjgh  hhhshfhsdhfhdfdfdfdf
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
