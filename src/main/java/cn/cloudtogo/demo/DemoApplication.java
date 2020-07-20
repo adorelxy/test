d4a9c653cfa94f51452e90688eb668c24e254643
xingyao.liu <xingyao.liu@cloudtogo.cn>
2020-07-20 17:39:58 +0800

    aa

--- src/main/java/cn/cloudtogo/demo/DemoApplication.java
+++ src/main/java/cn/cloudtogo/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        // hahdflj lfj
         SpringApplication.run(DemoApplication.class, args);
     }
 
